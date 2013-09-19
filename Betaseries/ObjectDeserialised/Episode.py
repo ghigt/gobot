9bf93b27f75a69382a64c413f31b09157ffd9719
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-09-19 17:42:48 +0200

    Séparation de la connexion dans une class, reste le mapping avec un ORM

--- Betaseries/ObjectDeserialised/Episode.py
+++ Betaseries/ObjectDeserialised/Episode.py
@@ -1,1 +1,16 @@
-__author__ = 'Nkio'
+__author__ = 'Alexandre Cloquet'
+
+
+class Episode(object):
+    def __init__(self, idBetaSerie, title, season, episode, show_id,
+                 show_title, code, description,
+                 date):
+        self.idBetaSerie = idBetaSerie
+        self.title = title
+        self.season = season
+        self.episode = episode
+        self.show_id = show_id
+        self.show_title = show_title
+        self.code = code
+        self.description = description
+        self.date = date
--- settings.py
+++ settings.py
@@ -1,1 +1,7 @@
-__author__ = 'Nkio'
+__author__ = 'Alexandre Cloquet'
+
+NAMEPROJECT = "Syli"
+
+API_KEY_BETASERIE = '3e803b0b5556'
+API_VERSION_BETASERIE = '2.1'
+HTTP = 'https'
