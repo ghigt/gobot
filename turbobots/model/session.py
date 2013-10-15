f4f936e9e347681130c276f16b1f30c5c7668174
Alexandre Cloquet <nkio@ubuntu.(none)>
2013-10-15 10:17:02 -0700

    Deleted

--- turbobots/model/session.py
+++
@@ -1,5 +0,0 @@
-from ming import Session
-from ming.orm import ThreadLocalORMSession
-
-mainsession = Session()
-DBSession = ThreadLocalORMSession(mainsession)
--- turbobots/websetup/schema.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Setup the TurboBots application"""
-from __future__ import print_function
-
-import logging
-from tg import config
-
-def setup_schema(command, conf, vars):
-    """Place any commands to setup turbobots here"""
