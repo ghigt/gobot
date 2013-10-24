8e7e71e20b2225921983aaef7a5225e669e34fc6
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-10-24 01:21:30 -0700

    modify all settings + now global settings and settings for each bot

--- AdminBot/Bot/Betaseries/Connexion.py
+++ AdminBot/Bot/Betaseries/Connexion.py
@@ -10,34 +10,52 @@ __status__ = "Development"
 import settings
 import logging
 import requests
+from AdminBot.Bot import settings as globalsettings
 from AdminBot.Bot.Adress.AdressBetaSeries import AdressBetaseries
 
 
 class Connexion:
+    """Connexion class help to connect Betaseries's Bot to Betaseries's API"""
+
     access_url = ((0, '://api.betaseries.com/shows/display/all.json?key='),
                   (1, '://api.betaseries.com/shows/display?key='),
-                  (2, '://api.betaseries.com/shows/episodes?key=%s&id=%s'))
+                  (2, '://api.betaseries.com/shows/episodes?key='))
 
     log = logging.getLogger("BetaSeries")
-    header = {'Accept': 'application/json', 'user-agent': settings.NAMEPROJECT}
+    header = {'Accept': 'application/json', 'user-agent': globalsettings.PROJECT_NAME_FOR_HTTP_HEADER}
 
     def __init__(self, http_type='http'):
         self.http_type = http_type
         self.adress = AdressBetaseries(http_type, settings.API_KEY_BETASERIE)
 
     def get_all_show(self):
+        """
+        Allow to have all show in Json file
+        @return None if Json file can't be decoded or list of show:
+        """
         self.adress.set_adress(self.access_url[0][1])
         r = requests.get(self.adress.get_new_address(), headers=self.header)
         self.log.info("Adresse interrogée = %s" % self.adress.get_new_address())
         if r.status_code == requests.codes.ok:
             self.log.info("Connexion à BetaSeries : Status de la page = %d" % r.status_code)
-            return r.json()['root']['shows']
+            try:
+                r.json()['root']['shows']
+            except ValueError:
+                self.log.error("Erreur sur le décodage du jSon, pass error")
+                return None
+            else:
+                return r.json()['root']['shows']
         else:
             self.log.error("Connexion à BetaSeries : Status de la page = %d" % r.status_code)
             r.raise_for_status()
             return False
 
     def get_each_show(self, id_show):
+        """
+        Allow to have one by one show in Json file
+        @param id_show: id from show to make a correct request
+        @return: Information about id show or None if Json file can't be decoded
+        """
         self.adress.set_adress(self.access_url[1][1])
         print self.adress.get_new_address() + "&id=%s" % id_show
         r = requests.get(self.adress.get_new_address() + "&id=%s" % id_show, headers=self.header)
@@ -49,7 +67,7 @@ class Connexion:
                 r.json()
             except ValueError:
                 self.log.error("Erreur sur le décodage du jSon, pass error")
-                pass
+                return None
             else:
                 return r.json()
         else:
@@ -57,6 +75,11 @@ class Connexion:
             return False
 
     def get_episode_from_id_show(self, id_show):
+        """
+        Allow to have all episodes from id_show in parameter
+        @param id_show: id from show to make a correct request
+        @return: All episodes about id show or None if Json file can't be decode
+        """
         self.adress.set_adress(self.access_url[2][1])
         r = requests.get(self.adress.get_new_address() + "&id=%s" % id_show, headers=self.header)
         self.log.info("Adresse interrogée = %s" % str(self.adress.get_new_address() + "&id=%s" % id_show))
