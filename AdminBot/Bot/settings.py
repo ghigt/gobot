7f30d741c9420e027c211cc25e9518b6b14808c0
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-10-24 01:24:48 -0700

    modifiy all setting for Bot to respect PP8

--- AdminBot/Bot/settings.py
+++ AdminBot/Bot/settings.py
@@ -5,4 +5,16 @@ __maintainer__ = "Alexandre Cloquet"
 __email__ = "Alexandre.cloquet@gmail.com"
 __status__ = "Development"
 
-HTTP = "HTTP"
+"""All information for all robots
+    We need to import all settings from all robots"""
+
+from AdminBot.Bot.Betaseries import settings as settings_betaserie
+
+#Generals Information
+
+PROJECT_NAME_FOR_HTTP_HEADER = "SyliBot"
+
+#Informations for Betaserie's Bot
+API_KEY_BETASERIE = settings_betaserie.API_KEY_BETASERIE
+API_VERSION_BETASERIE = settings_betaserie.API_VERSION_BETASERIE
+HTTP_MODE = settings_betaserie.HTTP_MODE
