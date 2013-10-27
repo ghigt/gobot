25bf0819163cb8e9d102cbc40dab09af00248f54
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-10-27 06:02:43 -0700

    Add new url + form + login + create new url

--- AdminBot/views.py
+++ AdminBot/views.py
@@ -2,11 +2,26 @@
 from django.http import HttpResponse
 import datetime
 
+from django.http import *
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
-    text = """<h1>Bienve sur mon blog !</h1>
-            <p>Les crepes bretonnes ca tue des mouettes en plein vol !</p>"""
-    return HttpResponse(text)
+    logout(request)
+    username = password = ''
+    print "coucou"
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+            return HttpResponseRedirect('/admin/')
+    return render_to_response('login.html', context_instance=RequestContext(request))
 
 
 def hello(request):
