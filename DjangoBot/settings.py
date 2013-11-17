8b1870a486329549e1e204add4decb772d0babcd
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-11-17 13:18:43 +0100

    Correction du path de la base pour le serveur

--- DjangoBot/settings.py
+++ DjangoBot/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/Nkio/PycharmProjects/DjangoBot/AdminBot/data.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'data.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
     },
     #'test': {
