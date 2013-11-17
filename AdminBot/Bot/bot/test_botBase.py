5464c9e91058ab0aed4f2503f052d1acc605fe51
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-11-17 16:16:24 +0100

    Ajout de test pour une class Abstraite .... assert 1 == 1 ...

---
+++ AdminBot/Bot/bot/test_botBase.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from AdminBot.Bot.bot.BotBase import BotBase
+
+__author__ = 'Alexandre Cloquet'
+__credits__ = ["Alexandre Cloquet"]
+__version__ = "0.1"
+__maintainer__ = "Alexandre Cloquet"
+__email__ = "Alexandre.cloquet@gmail.com"
+__status__ = "Development"
+
+
+class TestBotBase(TestCase):
+    def test_get_api_key(self):
+        """
+        Can't make abstract class
+        """
+        assert 1 == 1
+
+    def test_search(self):
+        """
+        Can't make abstract class
+        """
+        assert 1 == 1
+
+    def test_get_info_for(self):
+        """
+        Can't make abstract class
+        """
+        assert 1 == 1
+
+    def test_get_logger(self):
+        """
+        Can't make abstract class
+        """
+        assert 1 == 1
+
+    def test_register_bot(self):
+        """
+        Can't make abstract class
+        """
+        assert 1 == 1
