d8c5a75455ed3bfbadb1777206f761fbaf034dc0
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-11-20 10:39:10 +0100

    debut des tests du model, ou comment perdre du temps

---
+++ AdminBot/test_model.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+__author__ = 'Alexandre Cloquet'
+__credits__ = ["Alexandre Cloquet"]
+__version__ = "0.1"
+__maintainer__ = "Alexandre Cloquet"
+__email__ = "Alexandre.cloquet@gmail.com"
+__status__ = "Development"
+
+
+class TestLog(TestCase):
+    pass
+
+
+class TestLogError(TestCase):
+    pass
+
+
+class TestLogInfo(TestCase):
+    pass
+
+
+class TestBot(TestCase):
+    pass
+
+
+class TestShow(TestCase):
+    pass
+
+
+class TestEpisode(TestCase):
+    pass
+
+
+class TestGenre(TestCase):
+    pass
+
+
+class TestGenreToShow(TestCase):
+    pass
+
+
+class TestMovie(TestCase):
+    pass
+
+
+class TestGenreToMovie(TestCase):
+    pass
