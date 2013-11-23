24d72ee3024fc18e1095b0df73b3cd96db868b1e
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-11-23 11:45:32 +0100

    Correction

--- DjangoBot/settings.py
+++ DjangoBot/settings.py
@@ -14,7 +14,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(__file__),"data.db"),                      # Or path to database file if using sqlite3.
+        #'NAME': "data.db",
+        'NAME': os.path.join(os.path.dirname(__file__),"data.db"),
+        # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
     },
     #'test': {
