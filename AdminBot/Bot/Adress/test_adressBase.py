e070986b816d9802e36f4f0ece45ade055e67f2f
Alexandre Cloquet <alexandre.cloquet@gmail.com>
2013-11-27 11:48:43 +0100

    Correction de test

--- AdminBot/Bot/Adress/test_adressBase.py
+++ AdminBot/Bot/Adress/test_adressBase.py
@@ -16,23 +16,23 @@ class TestAdressBase(TestCase):
                                   settings.API_KEY_BETASERIE, "://test")
 
     def test_get_http(self):
-        self.assertEqual(self.adresse.http, "https", "")
+        self.assertEqual(self.adresse.get_http(), "https", "")
 
     def test_set_http(self):
-        self.adresse.http = "HTTP"
+        self.adresse.set_http("HTTP")
         self.assertEqual(self.adresse.http, "HTTP", "")
 
     def test_get_api_key(self):
-        self.assertEqual(self.adresse.api_key,
+        self.assertEqual(self.adresse.get_api_key(),
                          settings.API_KEY_BETASERIE, "")
 
     def test_set_api_key(self):
-        self.adresse.api_key = "test"
+        self.adresse.set_api_key("test")
         self.assertEqual(self.adresse.api_key, "test", "")
 
     def test_get_adress(self):
-        self.assertEqual(self.adresse.address, "://test", "")
+        self.assertEqual(self.adresse.get_adress(), "://test", "")
 
     def test_set_adress(self):
-        self.adresse.address = "://Je suis un test"
+        self.adresse.set_adress("://Je suis un test")
         self.assertEqual(self.adresse.address, "://Je suis un test", "")
